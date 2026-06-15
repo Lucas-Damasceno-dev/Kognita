e19d7e707b1f7f25b8f8d5f34926a1e20bbe44cb
Lucas Damasceno <lucas.damasceno.dev@gmail.com>
2026-06-15 10:25:47 -0300

    feat: add Spring Boot backend generated by Initializr

---
+++ backend/src/main/java/com/kognita/KognitaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kognita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KognitaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KognitaBackendApplication.class, args);
+	}
+
+}
